ef5a640ff3397f48ca462b6a52c914e01b5ee6e1
Diego Auza <diego1199@hotmail.com>
2016-05-24 17:49:20 -0400

    SMS Module Included

---
+++ rails/wally/app/controllers/sms_sender_controller.rb
@@ -0,0 +1,21 @@
+require 'twilio-ruby'
+
+class SmsSenderController < ApplicationController
+
+  def sendSMS
+
+    number_to_send_to = "+59170422102"
+
+    twilio_sid = "AC1f9a753d8c904a37dc3bbfb75d2b735c"
+    twilio_token = "9405e3cd62e25f69ce93dd6b3b005b7a"
+    twilio_phone_number = "2054172029"
+
+    @twilio_client = Twilio::REST::Client.new twilio_sid, twilio_token
+
+    @twilio_client.account.sms.messages.create(
+        :from => "+1#{twilio_phone_number}",
+        :to => number_to_send_to,
+        :body => "This is an message. It gets sent to #{number_to_send_to}"
+    )
+  end
+end
---
+++ rails/wally/app/helpers/sms_sender_helper.rb
@@ -0,0 +1,2 @@
+module SmsSenderHelper
+end
