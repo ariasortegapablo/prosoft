69f69a1189a527fc746517beab89c49685b0007c
Raxorg <lrdolucho@gmail.com>
2016-05-27 11:40:40 -0700

    Incluyendo cambios en la base de datos

---
+++ rails/wally/db/migrate/20160525221455_documents.rb
@@ -0,0 +1,11 @@
+class Documents < ActiveRecord::Migration
+  def change
+    create_table :documents do |t|
+      t.string :name
+      t.text :description
+      t.string :image
+
+      t.timestamps null: false
+    end
+  end
+end
