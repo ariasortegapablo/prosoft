a076944c9fbf1830415e2b7164d8f52568600231
pablow33cao <pablitoariasortega@gmail.com>
2016-05-31 23:50:02 +0200

    Integracion y Registros

--- rails/wally/app/controllers/informer_controller.rb
+++ rails/wally/app/controllers/informer_controller.rb
@@ -7,17 +7,18 @@ class InformerController < ApplicationController
     @dato = Informer.new(params[:post].permit(:name,:lastname,:phone,:email,:docutype,:identification))
 
     respond_to do |format|
-      if @dato.save
-       
+    #  if @dato.save
+     if @dato.validate
      #format.html { redirect_to :action => :show, :id =>@dato.id}
-       format.html { redirect_to url_for(:controller => :missing,:action => :new)}
+       format.html { redirect_to url_for(:controller => :missing,:action => :new,:param_name =>@dato.name ,
+      :param_lastname => @dato.lastname,:param_phone => @dato.phone, :param_email=>@dato.email,:param_docutype =>@dato.docutype,:param_i=>@dato.identification)}
+  
       else
-         format.html { render :new }
+         format.html { render :newinformer }
          
       end
     end
   end
-   def show 
-      end
+   
   
 end
