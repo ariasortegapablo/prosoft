fc1f80fef93038c4ddeb96e723f96026b54f9fa5
luciaparedes9 <luciaparedesmendoza@gmail.com>
2016-06-02 12:41:34 -0400

    Twitter notif

---
+++ rails/wally/test/controllers/twitter_controller_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class TwitterControllerControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
---
+++ rails/wally/test/controllers/twitter_pic_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class TwitterPicControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
